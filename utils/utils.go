7f23f90cefd0e55f19ef9c0343acea24d8cf8f2f
JPFrancoia <jeanpatrick.francoia@gmail.com>
2025-02-23 00:17:29 +0000

    Fixing 429 errors

--- utils/utils.go
+++ utils/utils.go
@@ -11,7 +11,24 @@ import (
 // The URL should be pre-formatted.
 // This function panics if any error occurs.
 func QueryApi(url string) []byte {
-	r, err := http.Get(url)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// User-agent taken from here:
+	// https://stackoverflow.com/questions/79450295/yfinance-429-client-error-too-many-requests-for-url
+	// This is to avoid 429 errors
+	req.Header.Set(
+		"User-Agent",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+	)
+
+	r, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if err != nil {
 		log.Fatal(err)
